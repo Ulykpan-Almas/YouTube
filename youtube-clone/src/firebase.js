108089012743bcb09586df5475156322eb1a161a
Your Name <Aulykpan@outlook.com>
2021-09-13 19:21:55 +0600

    first commit

---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA4Z3wM_tOLkwMxXcyLUWC16Rnn8c-Kip0",
+    authDomain: "clone-917c0.firebaseapp.com",
+    projectId: "clone-917c0",
+    storageBucket: "clone-917c0.appspot.com",
+    messagingSenderId: "1099114156421",
+    appId: "1:1099114156421:web:e220ba75b75f4e2233f4ec",
+    measurementId: "G-J383VBQCLF"
+  };
